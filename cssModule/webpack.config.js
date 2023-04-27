968bc34c64490f02abd9b1260fe9fbc942192ed7
yoonlang <cdt9473@gmail.com>
2023-04-27 14:57:31 +0900

    gitignore 적용, webpack css-loader 설정 테스트

--- cssModule/webpack.config.js
+++ cssModule/webpack.config.js
@@ -7,4 +7,19 @@ module.exports = {
     path: path.resolve(__dirname, "./dist"),
   },
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+    ],
+  },
 };
