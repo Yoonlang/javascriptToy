b3f63f24fe71dd29ea666917b02bba832fe963f8
yoonlang <cdt9473@gmail.com>
2023-05-10 16:57:00 +0900

    form 처리 완료

--- personal-record/src/pages/SignUpPage.jsx
+++ personal-record/src/pages/SignUpPage.jsx
@@ -1,6 +1,18 @@
 import Header from "components/Header";
+import { useForm } from "react-hook-form";
 
 const SignUpPage = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    console.log(data);
+    alert("등록");
+  };
+
   return (
     <>
       <Header />
@@ -8,15 +20,53 @@ const SignUpPage = () => {
         <div className="content_title">
           <h1> Sign Up, GreatPeoPle!</h1>
         </div>
-        <div id="form_container">
+        <form id="form_container" onSubmit={handleSubmit(onSubmit)}>
           <span className="form_elem">
-            <input id="name" placeholder="이름" />
+            <input
+              id="name"
+              placeholder="이름"
+              {...register("name", {
+                minLength: 2,
+                maxLength: 4,
+                required: true,
+                pattern: /^[ㄱ-ㅎㅏ-ㅣ가-힣]*$/,
+              })}
+            />
           </span>
+          {errors.name && <p>2~4 글자의 한글만 입력이 가능합니다.</p>}
           <span className="form_elem">
-            <input id="email" placeholder="이메일" />
+            <input
+              id="email"
+              placeholder="이메일"
+              {...register("email", {
+                required: true,
+                pattern: /^[a-zA-Z0-9]+@grepp.co$/,
+              })}
+            />
           </span>
+          {errors.email && (
+            <p>
+              이메일 ID는 영문(대소문자 구분 없음)과 숫자만 입력이 가능하며,
+              @grepp.co 형식의 이메일만 입력이 가능합니다.
+            </p>
+          )}
           <span className="form_elem">
-            <select id="role" name="role">
+            <input
+              id="nickname"
+              placeholder="닉네임"
+              {...register("nickname", {
+                minLength: 3,
+                maxLength: 10,
+                required: true,
+                pattern: /^[a-zA-Z]/,
+              })}
+            />
+          </span>
+          {errors.nickname && (
+            <p>대소문자 구분 없이 3~10 글자의 영문만 입력이 가능합니다.</p>
+          )}
+          <span className="form_elem">
+            <select id="role" {...register("role")}>
               <option value="">직군을 선택해주세요</option>
               <option value="backend">백엔드</option>
               <option value="frontend">프론트엔드</option>
@@ -26,7 +76,7 @@ const SignUpPage = () => {
           <span className="form_elem">
             <button type="submit">등록</button>
           </span>
-        </div>
+        </form>
       </main>
     </>
   );
